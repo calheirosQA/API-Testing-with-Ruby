aaaf80f371f27f5405e18e880a49bd86138828de
Lima <thiago.calheiros@philips.com>
2020-10-22 16:32:36 -0300

    delete all

--- features/support/env.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'cucumber'
-require 'httparty'
-require 'httparty/request'
-require 'httparty/response/headers'
-require 'rspec'
-require 'faker'
-require 'json'
